ac8e30cafedb905a1028b4dcb37d330dd9153a7b
Mitchell <mitchddd16@gmail.com>
2023-04-08 13:57:25 -0400

    Update scripts.js
    added scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 alert('Hello world');
+
+let myName = 'Jhon Doe';
+document.write(myName);
+myName = 'Jhon Doe Jr';
+document.write(myName);
+
+let simpleAdittion = 2 + 2;
+document.write(simpleAdittion);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
