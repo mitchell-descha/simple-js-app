e37b86989bd9c6867b9fd180d92a70f065210475
Mitchell <mitchddd16@gmail.com>
2023-06-22 00:36:21 -0400

    Updated commas

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,31 @@ let pokemonList = [
     {
         name: 'bulbasaur',
         pokemonNumber: 1,
-        type: ['grass', 'poison']
+        type: ['grass', 'poison'],
         height: 1.1
     },
     {
         name: 'charmander',
         pokemonNumber: 2,
-        type: ['fire']
+        type: ['fire'],
         height: 1.2
     },
     {
         name: 'squirtle',
         pokemonNumber: 3,
-        type: ['water']
+        type: ['water'],
         height: 1.3
     },
     {
         name: 'pikachu',
         pokemonNumber: 4,
-        type: ['electric']
+        type: ['electric'],
         height: 1.4
     },
     {
         name: 'eevee',
         pokemonNumber: 5,
-        type: ['normal']
+        type: ['normal'],
         height: 1.5
     },
 ];
